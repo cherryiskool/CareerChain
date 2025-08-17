4c821402ca1ae4a2fd9e3996a2200c2e93bb6311
Vincent <cherryiskool@gmail.com>
2025-08-17 17:39:09 +0100

    fixed default contract address bug

--- models/authModel.js
+++ models/authModel.js
@@ -4,7 +4,7 @@ const bcrypt = require('bcrypt');
 exports.registerUserToDatabase = async (username, password, walletAddress) => {
     const hashedPassword = await bcrypt.hash(password, 10);
     return await db.query('INSERT INTO users (username, hashedPassword, walletAddress, contractAddress, pfp) \
-        VALUES (?, ?, ?, 0x0000000000000000000000000000000000000000, "Default_pfp.jpg")', [
+        VALUES (?, ?, ?, "0x0000000000000000000000000000000000000000", "Default_pfp.jpg")', [
             username, 
             hashedPassword,
             walletAddress
